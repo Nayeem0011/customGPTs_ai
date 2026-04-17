df40a9d60576ee13aa93f8a8a98c2f7e77bf3b81
Nayeem0011 <sknayeemislam384@gmail.com>
2026-04-18 00:23:13 +0600

    customGPTs_ai

---
+++ app/component/Header.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import NavBar from './NavBar'
+import HeroPage from './HeroPage'
+
+const Header = () => {
+  return (
+    <div>
+      <NavBar />
+      <HeroPage />
+    </div>
+  )
+}
+
+export default Header
+
