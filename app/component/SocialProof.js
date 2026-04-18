4dc52dba6eecb006cb0dde79b744694ac2918336
Nayeem0011 <sknayeemislam384@gmail.com>
2026-04-19 02:16:09 +0600

    SocialProof file compeleted

--- app/component/SocialProof.js
+++ app/component/SocialProof.js
@@ -1,8 +1,8 @@
 'use client';
 
 import Marquee from "react-fast-marquee";
-import Image from "next/image";
 
+import Image from "next/image";
 import company_logo from "../../public/company_logo.svg.svg";
 import company_logo1 from "../../public/company_logo1.svg.svg";
 import company_logo2 from "../../public/company_logo2.svg.svg";
@@ -12,26 +12,42 @@ import company_logo5 from "../../public/company_logo5.svg.svg";
 import company_logo6 from "../../public/company_logo6.svg.svg";
 import company_logo7 from "../../public/company_logo7.svg.svg";
 
-const SocialProof = () => {
+const logos = [
+  company_logo,
+  company_logo1,
+  company_logo2,
+  company_logo3,
+  company_logo4,
+  company_logo5,
+  company_logo6,
+  company_logo7,
+];
 
+const SocialProof = () => {
   return (
-    <section>
-      <div className="">
-        <p className="text-[#050E14] font-medium text-[18px] leading-[160%] tracking-[0%] text-center">
-          Trusted by leading organizations around the world
+    <section className="flex flex-col items-center gap-10 mb-20">
+      <div>
+        <p className="text-[#42424F] font-['Geist'] font-medium text-[20px] leading-[160%] tracking-[0em] text-center align-middle uppercase">
+          Trusted by 10,000+ organizations worldwide
         </p>
       </div>
-      <div className="w-210.75 h-10 flex items-center gap-10 mx-auto mt-8 mb-4">
-        <Marquee speed={60} gradient={false} autoFill={false}>
-          <div className="flex items-center justify-center gap-10">
-            <Image src={company_logo} alt="company_logo" className="h-10 w-25.75" />
-            <Image src={company_logo1} alt="company_logo1" className="h-10 w-25.75" />
-            <Image src={company_logo2} alt="company_logo2" className="h-10 w-25.75" />
-            <Image src={company_logo3} alt="company_logo3" className="h-10 w-25.75" />
-            <Image src={company_logo4} alt="company_logo4" className="h-10 w-25.75" />
-            <Image src={company_logo5} alt="company_logo5" className="h-10 w-25.75" />
-            <Image src={company_logo6} alt="company_logo6" className="h-10 w-25.75" />
-            <Image src={company_logo7} alt="company_logo7" className="h-10 w-25.75" />
+
+      <div className="max-w-7xl mx-auto w-full px-6">
+        <Marquee speed={50} gradient={false} autoFill={false}>
+          <div className="flex items-center justify-center gap-6">
+            {logos.map((logo, index) => (
+              <div key={index} className="flex items-center gap-6">
+                <div className="w-px h-6 bg-[#E5E5E5]" />
+                <Image
+                  src={logo}
+                  alt="Company logo"
+                  className="h-10.5 w-31.25 object-contain opacity-100 hover:opacity-80 transition cursor-pointer"
+                />
+              </div>
+            ))}
+
+            {/* last separator (optional same behavior) */}
+            <div className="w-px h-6 bg-[#E5E5E5]" />
           </div>
         </Marquee>
       </div>
@@ -40,3 +56,4 @@ const SocialProof = () => {
 };
 
 export default SocialProof;
+
