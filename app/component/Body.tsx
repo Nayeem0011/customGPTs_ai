5e56ad3471898de0d6fd176ee29843691fc12e2b
Nayeem0011 <sknayeemislam384@gmail.com>
2026-04-19 19:40:40 +0600

    Trusted file aded

--- app/component/Body.tsx
+++ app/component/Body.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Lunch from './Lunch'
 import SocialProof from "./SocialProof"
+import Trusted from './Trusted'
 
 const Body = () => {
   return (
     <div>
       <SocialProof />
+      <Trusted />
       <Lunch />
     </div>
   )
---
+++ app/component/Trusted.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const Trusted = () => {
+    return (
+        <div className='max-w-7xl mx-auto w-full px-6 h-screen'>
+            <div className='flex items-center justify-between'>
+                <h1 className='text-[#9898A7] font-medium text-[48px] leading-[130%] tracking-[0px] align-middle'>
+                    Why startup choose <br /> <span className='text-[#4A4A59]'>CustomGPT.ai</span>
+                </h1>
+                <p className='text-[#8B929E] font-normal text-[16px] leading-[160%] tracking-[0px] align-middle'>
+                    Built for organizations. AI that’s easy, secure, <br /> and represents us like a star team member.
+                </p>
+            </div>
+            <div className='flex items-center justify-center gap-6'>
+                <div className='w-149.5 h-115.5 rounded-4xl bg-[radial-gradient(120.74%_124.92%_at_7.26%_100%,#326DE4_0%,#628FFD_100%),radial-gradient(167.08%_140.48%_at_79.5%_0%,rgba(255,255,255,0.06)_0%,rgba(255,255,255,0)_100%)] shadow-[inset_1.01px_0.94px_4.09px_0px_#FFFFFF21,inset_1.9px_1.77px_8.17px_0px_#FFFFFF21]'>
+
+                </div>
+                <div className='w-149.5 h-115.5 rounded-4xl bg-[#F2F3FA] shadow-[inset_1.01px_0.94px_4.09px_0px_#FFFFFF21,inset_1.9px_1.77px_8.17px_0px_#FFFFFF21]'>
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Trusted
