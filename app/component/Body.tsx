8bccdeffeaa843ff182a460098d1398dc2ccb0e8
Abdullah <mabdullah097@gmail.com>
2026-04-19 22:34:10 +0600

    fix: footer layout structure

--- app/component/Body.tsx
+++ app/component/Body.tsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import Lunch from './Lunch'
 import SocialProof from "./SocialProof"
+import Driver from './Driver'
+import Footer from './Footer'
+
 
 const Body = () => {
   return (
     <div>
       <SocialProof />
       <Lunch />
+      <Driver />
+      <Footer/>
+
     </div>
   )
 }
